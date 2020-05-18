f4c4029509ac45d26dbcf50c9a6d5a284ff8c2ec
Ettore Aquino <ettore.aquino@truecomercializadora.com>
2020-05-18 20:42:31 -0300

    Nova funcao que retorna os decks de um estudo agrupados por data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='truecomercializadora',
-    version='0.1.15',
+    version='0.1.16',
     description='A comprehensive library to centralize the main functions used across applications and services',
     url='https://github.com/truecomercializadora/truecomercializadora.git',
     author='Ettore Aquino',
--- truecomercializadora/encadeado.py
+++ truecomercializadora/encadeado.py
@@ -8,23 +8,47 @@ import zipfile as zp
 
 
 def get_decks_estudo(estudo_zip):
-  """
-  Um estudo pode ser composto por um ou mais decks. Estudos prospectivos sao
-    em geral, compostos por pares mensais de decks. Um NEWAVE outro DECOMP. A
-    funcao 'get_decks_estudo' retorna um dicionario contendo os decks dispo-
-    niveis dentro do estudo, agrupados em 'decomp' ou 'newave'. 
-  """
-  
-  if type(estudo_zip) != zp.ZipFile:
-    raise Exception("'get_estudo_files' should be a valid zipfile.ZipFile \
-    Class. Input has type {}".format(type(estudo_zip)))
-  
-  return {
-    'decomp': list({os.path.split(file)[0] for file in estudo_zip.namelist()
-                    if 'DC' in file}),
-    'newave': list({os.path.split(file)[0] for file in estudo_zip.namelist()
-                    if 'NW' in file})
-  }
+	"""
+	Um estudo pode ser composto por um ou mais decks. Estudos prospectivos sao
+	em geral, compostos por pares mensais de decks. Um NEWAVE outro DECOMP. A
+	funcao 'get_decks_estudo' retorna um dicionario contendo os decks dispo-
+	niveis dentro do estudo, agrupados em 'decomp' ou 'newave'. 
+	"""
+
+	if type(estudo_zip) != zp.ZipFile:
+		raise Exception("'get_estudo_files' should be a valid zipfile.ZipFile \
+		Class. Input has type {}".format(type(estudo_zip)))
+
+	return {
+		'decomp': list({os.path.split(file)[0] for file in estudo_zip.namelist()
+						if 'DC' in file}),
+		'newave': list({os.path.split(file)[0] for file in estudo_zip.namelist()
+						if 'NW' in file})
+	}
+
+def get_decks_por_data(estudo_zip):
+    """
+    Um estudo pode ser composto por um ou mais decks. Estudos prospectivos sao
+      em geral, compostos por pares mensais de decks. Um NEWAVE outro DECOMP. A
+      funcao 'get_decks_por_data' retorna um dicionario contendo os decks dispo-
+      niveis dentro do estudo, agrupados pela data do deck. 
+    """
+    
+    if type(estudo_zip) != zp.ZipFile:
+        raise Exception("'get_decks_por_data' should be a valid zipfile.ZipFile \
+        Class. Input has type {}".format(type(estudo_zip)))
+
+    datas = list(set([os.path.split(file)[0].split('-')[0][2:] for file in estudo_zip.namelist()]))
+    D = {}
+    for data in datas:
+        D.update({data: {}})
+        deck_nw = {os.path.split(file)[0] for file in estudo_zip.namelist() if data in file and 'NW' in file}
+        deck_dc = {os.path.split(file)[0] for file in estudo_zip.namelist() if data in file and 'DC' in file}
+        if len(deck_nw)>0:
+            D[data].update({"NW": list(deck_nw)[0]})
+        if len(deck_dc)>0:
+            D[data].update({"DC": list(deck_dc)[0]})
+    return D
 
 def get_estudo_files(estudo_zip):
   """
