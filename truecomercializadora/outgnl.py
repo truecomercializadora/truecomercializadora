3e1730be7ea3ff6b66125d6a16de56c87f2104a4
Ettore Aquino <ettore.aquino@truecomercializadora.com>
2020-06-18 23:58:00 -0300

    Funcao para retornar dataframe a paritr do registro tg do outgnl

--- truecomercializadora/outgnl.py
+++ truecomercializadora/outgnl.py
@@ -1,6 +1,7 @@
 """
 Modulo desenhado para conter as classes e funcoes relacionadas ao arquivo outgnl
 """
+import pandas as pd
 
 from . import utils_files
 
@@ -91,3 +92,38 @@ def get_registro_gl(outgnl_str: str) -> str:
     registro_gl = '\r\n'.join(registro_gl.splitlines()[2:])
 
     return registro_gl
+
+def get_df_from_registro_tg(registro_tg_str: str) -> pd.DataFrame:
+    """
+    Retorna o pd.DataFrame correspondente ao Registro TG informado.
+     A funcao devera receber o Registro TG ja no formato string
+    """
+    if type(registro_tg_str) != str:
+        raise Exception("'get_df_from_registro_tg' can only receive a string. "
+                        "{} is not a valid input type".format(type(registro_tg_str)))
+
+    if 'Usina           Est           Pat 1               Pat 2               Pat3' not in registro_tg_str:
+        raise Exception("Input string does not seem to represent a Registro TG"
+                        "string. Check the input")
+
+    # Interando pelas linhas do registro e estruturando elas como dicionarios     
+    L = []
+    for line in registro_tg_str.splitlines()[4:]:
+        L.append({
+            "bloco": line [:3].strip(),
+            "cod_usina": int(line[4:8].strip()),
+            "nome_usina": line[14:25].strip(),
+            "ip": int(line[25:27].strip()),
+            "inflex_1": float(line[30:34].strip()),
+            "disp_1":float(line[34:40].strip()),
+            "cvu_1": float(line[40:50].strip()),
+            "inflex_2": float(line[50:54].strip()),
+            "disp_2":float(line[54:60].strip()),
+            "cvu_2": float(line[60:70].strip()),
+            "inflex_3": float(line[70:74].strip()),
+            "disp_3":float(line[74:80].strip()),
+            "cvu_3": float(line[80:].strip()),
+        })
+    
+    # Retornando a lista de dicionarios como um pd.DataFrame     
+    return pd.DataFrame(L)
