3d08f0790690d7001b5208fc0674828a1b8cb066
Ettore Aquino <ettore.aquino@truecomercializadora.com>
2020-08-26 16:24:12 -0300

    Quebra de linha ajustada para registro DP

--- truecomercializadora/dadger.py
+++ truecomercializadora/dadger.py
@@ -27,8 +27,11 @@ def get_registro_dp(dadger_str: str) -> str:
     end = "BLOCO 7"
     registro_dp = utils_files.select_document_part(dadger_str, begin, end)
     
-    # eliminando as linhas antes e depois dos dados     
-    registro_dp = '\r\n'.join(registro_dp.splitlines()[2:-2])
+    # eliminando as linhas antes e depois dos dados
+    if '\r\n' in dadger_str:
+        registro_dp = '\r\n'.join(registro_dp.splitlines()[2:-2])
+    else:
+        registro_dp = '\n'.join(registro_dp.splitlines()[2:-2])
 
     return registro_dp
 
