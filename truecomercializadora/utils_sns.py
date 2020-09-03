b0fd47dacd74c2844af7a2aa91105e121dbf4040
Ettore Aquino <ettore.aquino@truecomercializadora.com>
2020-09-03 00:43:25 -0300

    Versao da funcao de publicacao SNS considerando message attributes

--- truecomercializadora/utils_sns.py
+++ truecomercializadora/utils_sns.py
@@ -2,18 +2,32 @@ import boto3
 import json
 import os
 
-def publish_message(message, TOPIC_ARN):
-  """
-  # ============================================================================================ #
-  # Publish a message to a SNS TOPIC. Make sure you have proper credentials installed in your    #
-  #   machine which can be done using the aws cli:                                               #
-  #    https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html                  #
-  # 'message' should of type (list, dict or str) and 'TOPIC_ARN'  should be a string representing#
-  #   the TOPIC ARN to which the message should be published.                                    #
-  # ============================================================================================ #
-  """
-  sns_client = boto3.client('sns')
-  return sns_client.publish(
-    TopicArn = TOPIC_ARN,
-    Message = json.dumps(message)
-  )
+def publish_message(
+    message: dict,
+    TopicArn: str,
+    Subject: str=None,
+    MessageAttributes: str=None) -> dict:
+    """
+    Publish a message to a SNS TOPIC. Make sure you have proper credentials installed in your
+     machine which can be done using the aws cli:
+     https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
+     'message' should of type (list, dict or str) and 'TOPIC_ARN'  should be a string representing
+     the TOPIC ARN to which the message should be published.
+    """
+    sns_client = boto3.client('sns')
+    
+    if Subject != None and MessageAttributes==None:
+        raise Exception("Subject defined but MessageAttributes == None")
+    
+    if MessageAttributes:
+        return sns_client.publish(
+            TopicArn = TopicArn,
+            Message = json.dumps(message),
+            Subject=Subject,
+            MessageAttributes=MessageAttributes
+        )
+    
+    return sns_client.publish(
+            TopicArn = TopicArn,
+            Message = json.dumps(message)
+        )
