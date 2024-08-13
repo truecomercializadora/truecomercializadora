95d05c8d2f2241f7e82759391c18fa61ff74c254
Juliana Ribeiro <juliana.ribeiro@truecomercializadora.com>
2024-08-13 10:02:44 -0300

    atualizando dependencias no setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='truecomercializadora',
-    version='0.4.9',
+    version='0.5.0',
     description='A comprehensive library to centralize the main functions used across applications and services',
     url='https://github.com/truecomercializadora/truecomercializadora.git',
     author='Ettore Aquino',
@@ -15,7 +15,9 @@ setup(
         'google-auth-httplib2==0.1.1',
         'google-auth-oauthlib==0.4.6',
         'workalendar==17.0.0',
-        'pyTelegramBotAPI==4.20.0'
+        'pyTelegramBotAPI==4.20.0',
+        'urllib3==2.2.2',
+        'requests==2.32.3'
     ],
     classifiers=[
         'Development Status :: 0 - Beta',
