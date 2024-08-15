c7848633ffc46493a62ee52a85b2875ba59bda91
brunamatsumoto <bruna.matsumoto@truecomercializadora.com>
2024-08-15 13:49:59 -0300

    adicionando build_ena_dict_from_prevs no ena.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='truecomercializadora',
-    version='1.2.2',
+    version='1.2.3',
     description='A comprehensive library to centralize the main functions used across applications and services',
     url='https://github.com/truecomercializadora/truecomercializadora.git',
     author='Ettore Aquino',
