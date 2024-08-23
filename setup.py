b7e5e7103adfcd881ebb29232f9e8a2de58b07d0
brunamatsumoto <bruna.matsumoto@truecomercializadora.com>
2024-08-23 16:28:26 -0300

    editando funcao de leitura do re.dat para nao arredondar valores das travas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='truecomercializadora',
-    version='1.2.3',
+    version='1.2.4',
     description='A comprehensive library to centralize the main functions used across applications and services',
     url='https://github.com/truecomercializadora/truecomercializadora.git',
     author='Ettore Aquino',
